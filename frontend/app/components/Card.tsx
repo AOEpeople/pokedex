e2529a0c6490212cc7f6114593d4ab898122c72a
Achim Rolle <achim.rolle@aoe.com>
2022-06-27 08:33:37 +0200

    feat(live-coding): prepare live coding

--- frontend/app/components/Card.tsx
+++ frontend/app/components/Card.tsx
@@ -1,37 +1,11 @@
-import { Form, useSubmit } from "@remix-run/react";
+import { Form } from "@remix-run/react";
 
-import { Pokemon } from "~/graphql/sdk";
-
-export const Card = ({ id, name, catched }: Pokemon) => {
-  const submit = useSubmit();
+export const Card = () => {
   const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
 
   return (
     <Form method="post" className="card">
-      <input type="hidden" name="id" value={id} />
-      <input
-        id={`${name}-catched`}
-        className="checkbox"
-        type="checkbox"
-        name="catched"
-        defaultChecked={catched}
-        onChange={(e) => submit(e.currentTarget.form)}
-      />
-      <noscript>
-        <button type="submit">Submit</button>
-      </noscript>
-
-      <label className="cursor-pointer" htmlFor={`${name}-catched`}>
-        <img
-          width={96}
-          height={96}
-          className="mx-auto"
-          src={imageUrl}
-          alt={name}
-        />
-        <strong className="capitalize">{name}</strong>{" "}
-        <span className="text-gray-500 text-sm">#{id}</span>
-      </label>
+      pokemon hier
     </Form>
   );
 };
